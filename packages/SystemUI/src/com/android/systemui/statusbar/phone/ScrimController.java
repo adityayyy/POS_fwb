04806e7a8b120b4550200b6d0a56737f9a99f59a
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:24:58 -0700

    SystemUI: Follow light/dark theme in quick settings
    Android 12's dual-tone style where the quick settings panel is always
    dark makes the light theme look like a second-class citizen. Pure black
    also looks out-of-place next to QS tiles and the notification panel
    because dynamic themes don't affect it at all.
    
    Revert to the ~Beta 1 style where quick settings used the same theme as
    the notification shade.
    
    - colorAccentPrimary has been replaced with colorAccent for contrast in
      light mode, because colorAccentPrimary is system_accent1_100 (dark
      accent color)
    - Footer chips have been converted to surfaces (similar to QS tiles and
      notifications), which makes more sense with the new style
    - The QS background is now the same as the notification shade background
      in both light and dark modes
    
    Demo screenshots (with dual-tone commit):
    https://twitter.com/kdrag0n/status/1445922541218922496
    
    [kdrag0n: ported to 12L]
    [neobuddy89: adapted for 14QPR1]
    
    Saalim:
    - Use colorAccents in shade values to match systme theme design change
    
    [adithya2306: Adapt to 15-qpr0]
    
    Change-Id: I3d45b72f0f119e100505409d178ab8f698993881
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -1005,6 +1005,8 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                 mNotificationsTint = behindTint;
                 mBehindAlpha = 1;
                 mBehindTint = Color.BLACK;
+                mBehindAlpha = mCustomScrimAlpha;
+                mBehindTint = Color.TRANSPARENT;
             } else {
                 mBehindAlpha = behindAlpha;
                 if (mState == ScrimState.KEYGUARD && mTransitionToFullShadeProgress > 0.0f) {
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -79,9 +79,10 @@ public enum ScrimState {
             } else {
                 mAnimationDuration = ScrimController.ANIMATION_DURATION;
             }
-            mFrontTint = mBackgroundColor;
-            mBehindTint = mBackgroundColor;
-            mNotifTint = mClipQsScrim ? mBackgroundColor : Color.TRANSPARENT;
+            // make sure we only darken the wallpaper
+            mFrontTint = Color.BLACK;
+            mBehindTint = Color.BLACK;
+            mNotifTint = mClipQsScrim ? Color.BLACK : Color.TRANSPARENT;
 
             mFrontAlpha = 0;
             mBehindAlpha = mClipQsScrim ? 1 : mScrimBehindAlphaKeyguard;
@@ -128,6 +129,7 @@ public enum ScrimState {
         public void prepare(ScrimState previousState) {
             mBehindAlpha = mClipQsScrim ? 1 : mDefaultScrimAlpha;
             mBehindTint = mClipQsScrim ? mBackgroundColor : mSurfaceColor;
+            mBehindTint = mSurfaceColor;
             mNotifAlpha = mClipQsScrim ? mDefaultScrimAlpha : 0;
             mNotifTint = Color.TRANSPARENT;
             mFrontAlpha = 0f;
@@ -189,7 +191,7 @@ public enum ScrimState {
             final boolean isDocked = mDockManager.isDocked();
             mBlankScreen = mDisplayRequiresBlanking;
 
-            mFrontTint = mBackgroundColor;
+            mFrontTint = Color.BLACK;
             mFrontAlpha = (alwaysOnEnabled || isDocked || quickPickupEnabled)
                     ? mAodFrontScrimAlpha : 1f;
 
